28d4963362d655cf27ee42409c704b14bc8be757
zwyeo <>
2022-07-13 00:43:11 +0800

    configured navbar component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
-function App() {
-  return <div>App</div>;
-}
+import React, { useState } from "react";
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Routes from "./components/Routes";
+
+const App = () => {
+  const [darkTheme, setDarkTheme] = useState(false);
+
+  return (
+    <div className={darkTheme ? "dark" : ""}>
+      <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
+        <Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme} />
+        <Routes />
+        <Footer />
+      </div>
+    </div>
+  );
+};
 
 export default App;
