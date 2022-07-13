53a44e00da1770581c4dc2d5f4fe63701660e9e7
zwyeo <>
2022-07-13 14:38:33 +0800

    added Results component

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -10,7 +10,7 @@ const Routes = () => {
         <Route exact path="/">
           <Redirect to="/search" />
         </Route>
-        <Route exact path={["/search", "/images", "/news", "/videos"]}>
+        <Route exact path={["/search", "/image", "/news", "/video"]}>
           <Results />
         </Route>
       </Switch>
---
+++ src/contexts/ResultContextProvider.js
@@ -0,0 +1,47 @@
+import React, { createContext, useContext, useState } from "react";
+
+const ResultContext = createContext();
+const baseUrl = "https://google-search3.p.rapidapi.com/api/v1";
+
+export const ResultContextProvider = ({ children }) => {
+  const [results, setResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("Javascript Mastery");
+
+  // /videos, /search, /image
+  const getResults = async (type) => {
+    setIsLoading(true);
+
+    const response = await fetch(`${baseUrl}${type}`, {
+      method: "GET",
+      headers: {
+        "X-User-Agent": "desktop",
+        "X-Proxy-Location": "US",
+        "X-RapidAPI-Key": "f5c14abe2cmshb1d2d4038bc3c9ep1676fajsn77fd71c56f9c",
+        "X-RapidAPI-Host": "google-search3.p.rapidapi.com",
+      },
+    });
+
+    const data = await response.json();
+
+    if (type.includes("/news")) {
+      setResults(data.entries);
+    } else if (type.includes("/images")) {
+      setResults(data.image_results);
+    } else {
+      setResults(data.results);
+    }
+
+    setIsLoading(false);
+  };
+
+  return (
+    <ResultContext.Provider
+      value={{ getResults, results, searchTerm, setSearchTerm, isLoading }}
+    >
+      {children}
+    </ResultContext.Provider>
+  );
+};
+
+export const useResultContext = () => useContext(ResultContext);
